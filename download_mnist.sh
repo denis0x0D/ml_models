1874cce070b47fcc7eb2f3b2fdc4944cae36be07
Denis Khalikov <dennis.khalikov@gmail.com>
2018-12-25 15:54:49 +0300

    Add mnist script and example

---
+++ download_mnist.sh
@@ -0,0 +1,5 @@
+mkdir -p data
+wget -P data http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+wget -P data http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+wget -P data http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+wget -P data http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
