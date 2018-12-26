9abf96db6f2289f377c00ecb31ad7958f0e018bf
Denis Khalikov <dennis.khalikov@gmail.com>
2018-12-26 14:43:44 +0300

    Run memory scheduler tests

---
+++ build_test.sh
@@ -0,0 +1,1 @@
+bazel build --config=opt //tensorflow/compiler/xla/service:hlo_memory_scheduler_test
---
+++ run_test.sh
@@ -0,0 +1,1 @@
+./bazel-bin/tensorflow/compiler/xla/service/hlo_memory_scheduler_test 2> /dev/null
